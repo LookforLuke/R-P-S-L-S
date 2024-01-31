2178fa033c42c347e3388c81cc174bcafb742a6c
LookforLuke <>
2024-01-31 01:34:34 +0000

    feat: add listening event for user selection

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "play-again") {
+                alert("You chose play again");
+            } else {
+                let userChoice = this.getAttribute("data-type");
+                runGame(userChoice);
+            }
+        });
+    }
+});
