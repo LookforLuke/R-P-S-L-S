bff7e61a12d3f7d66797e2598aaf80bcd052a2f0
LookforLuke <>
2024-01-31 01:59:46 +0000

    feat: add confirm choices log

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,10 +13,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+function runGame(userChoice) {
 //number to choice mapping
 var pc_win, mapping, pcChoice, pc_choice_output, tie, user_win, userChoice, user_choice_output;
 mapping = ["rock", "paper", "scissors", "spock", "lizard"];
 
 //generate pc choice
 pc_choice = Math.floor(Math.random() * 5);
-pc_choice_output = "I chose %s." % mapping[pc_choice];
+pc_choice_output = mapping[pc_choice];
+
+//confirm choices
+console.log(pc_choice_output);
+console.log(userChoice);
+}
