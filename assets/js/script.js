0160c5c808cd6a12d0732ac5cc755292dda45515
LookforLuke <>
2024-01-31 02:19:50 +0000

    feat: add user choice confirmation and display function

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,4 +25,32 @@ pc_choice_output = mapping[pc_choice];
 //confirm choices
 console.log(pc_choice_output);
 console.log(userChoice);
+
+//check user choice section
+if (userChoice === "scissors") {
+    displayResult(userChoice, pc_choice_output);
+} else if (userChoice === "paper") {
+    displayResult(userChoice, pc_choice_output);
+} else if (userChoice === "rock") {
+    displayResult(userChoice, pc_choice_output);
+} else if (userChoice === "lizard") {
+    displayResult(userChoice, pc_choice_output);
+} else if (userChoice === "spock") {
+    displayResult(userChoice, pc_choice_output);
+} else {
+    alert(`unknown user choice: ${userChoice}`);
+        throw `Unknown user choice: ${userChoice}. Aborting`;
+}
+}
+
+//display choices
+function determineChoices() {
+    let userpick = parseInt(document.getElementById('userpick').innerText);
+    let computerpick = parseInt(document.getElementById('computerpick').innerText);
+}
+
+//display Results
+function displayResult(userpick, computerpick) {
+    document.getElementById('userpick').textContent = userpick;
+    document.getElementById('computerpick').textContent = computerpick;
 }
